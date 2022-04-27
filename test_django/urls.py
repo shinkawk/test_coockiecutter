e2cf8d15cadc7a7c6f8cc6c70cb79978d30819a7
shinkawk <shinkawk@gmail.com>
2022-04-27 18:40:46 +0900

    urls

--- test_django/urls.py
+++ test_django/urls.py
@@ -24,6 +24,14 @@ from rest_framework import permissions
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("authentication.urls")),
+    path(
+        "api/",
+        include(
+            [
+                path("", include("authentication.api_urls")),
+            ]
+        ),
+    ),
 ]
 
 if settings.DEBUG:
@@ -46,4 +54,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     if settings.MEDIA_URL != "/":
         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
